7acbfc4bf69646c8cbac1ba24c055ba017a15ddc
Derrick Strong <derrickmstrong@gmail.com>
2020-12-06 09:51:26 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let acc = document.getElementsByClassName("accordion");
+let arrow = document.getElementsByClassName("arrow-down");
+
+for (let i = 0; i < acc.length; i++) {
+    // Toggle active class
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        
+    // Toggle panel class
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+        arrow[i].style.transform = "rotate(0deg)";
+    } else {
+        panel.style.display = "block"
+        arrow[i].style.transform = 'rotate(180deg)';
+    }
+
+    // Max-height
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+    })
+    
+}
